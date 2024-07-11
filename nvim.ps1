c462519171c5b6d3329851821394f2fdcf25c32e
Roland Halbaksz <roland.halbaksz@semilab.hu>
2024-07-11 11:00:09 +0200

    Improve sandbox mode.

--- nvim.ps1
+++ nvim.ps1
@@ -1,8 +1,8 @@
 [CmdletBinding()]
 Param()
 
-$Path = $PSScriptRoot
-$Cmd = "cd '$Path'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u '$Path/nvim/init.lua' --cmd 'set rtp+=$Path/nvim'"
+$Path = "$PSScriptRoot"
+$Cmd = "cd '$Path\nvim'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u 'init.lua' --cmd 'set rtp+=$Path/nvim'"
 Write-Verbose $Cmd
 
 pwsh -NoProfile -Command $Cmd
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,35 @@
 require("generic.set")
-require("generic.remap")
 require("generic.cmds")
 require("system.hiyank")
 
 if os.getenv("NVIM_PROFILE") == "bare" then
     require("bare.init")
 elseif os.getenv("NVIM_PROFILE") == "sandbox" then
-    print "Sandbox mode"
-    vim.env.XDG_CONFIG_HOME = "./.sandbox/config"
-    vim.env.XDG_DATA_HOME = "./.sandbox/local/share"
-    vim.env.XDG_STATE_HOME = "./.sandbox/.local/state"
-    vim.env.XDG_CACHE_HOME = "./.sandbox/.cache"
+    print ("Sandbox mode: ".. vim.fn.getcwd())
 
-    require("generic.lazy_init")
+    -- Get the current runtime paths
+    local rtp = vim.api.nvim_list_runtime_paths()
+
+    -- Filter out paths that start with '~' or 'C:\\Users'
+    local filtered_rtp = {}
+    for _, path in ipairs(rtp) do
+        if not path:match("^~") and not path:match("^C:\\Users") then
+            table.insert(filtered_rtp, path)
+        end
+    end
+
+    -- Rebuild the runtime path
+    vim.o.runtimepath = table.concat(filtered_rtp, ",")
+    print(vim.o.runtimepath)
+
+    vim.env.XDG_CONFIG_HOME = "../.sandbox/config"
+    vim.env.XDG_DATA_HOME = "../.sandbox/local/share"
+    vim.env.XDG_STATE_HOME = "../.sandbox/.local/state"
+    vim.env.XDG_CACHE_HOME = "../.sandbox/.cache"
+
+    require("generic.bootstrap_lazy")
 else
-    require("generic.lazy_init")
+    require("generic.bootstrap_lazy")
 end
 
+require("lua.generic.remap")
