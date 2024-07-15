96d68498079844b3f7cb217fcfb6228b11945db9
Roland Halbaksz <roland.halbaksz@semilab.hu>
2024-07-15 13:23:32 +0200

    A way more advanced sandbox mode.

--- nvim.ps1
+++ nvim.ps1
@@ -1,8 +1,79 @@
 [CmdletBinding()]
-Param()
+Param(
+    [Parameter(Mandatory = $false)] 
+    [string]
+    $Profile = "defalut"
+)
 
-$Path = "$PSScriptRoot"
-$Cmd = "cd '$Path\nvim'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u 'init.lua' --cmd 'set rtp+=$Path/nvim'"
-Write-Verbose $Cmd
+$Cwd = "$PWD" #.Replace('\','\\') 
+$CfgDir = "$PSScriptRoot" #.Replace('\','\\')  
+
+# $Cmd = "`$Env:SANDBOX_NVIM_PWD='$CWD'; cd '$Path'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u '$Path\nvim\init.lua' --cmd 'set rtp+=$Path/nvim'"
+# $Cmd = "cd '$CWD';`$Env:NVIM_CONFIG='$Path/nvim'; `$Env:NVIM_PROFILE='sandbox'; nvim -i NONE --cmd 'lua vim.env.XDG_CONFIG_HOME=`'$Path`''"
+
+
+
+# $Cmd = "& {
+#     `$Exec = {
+#         `$Profile = `$args[0]
+#         `$Cwd = `$args[1]
+#         `$CfgDir = `$args[2]
+#         # `$Cmd = 'let `$XDG_CONFIG_HOME=`$CfgDir/nvim'
+#         `$Cmd = 'lua vim.env.XDG_CONFIG_HOME=`$CfgDir/nvim'
+# 
+# 
+#         Write-Host `$Profile
+#         Write-Host `$Cwd
+#         Write-Host `$CfgDir
+#         Write-Host `$Cmd
+# 
+#         cd `$Cwd; 
+#         `$ENV:NVIM_PROFILE=`$Profile; 
+#         nvim -i NONE --cmd `$Cmd;
+#     }
+#     & `$Exec '$Profile' '$Cwd' '$CfgDir'
+# }"
+# 
+# pwsh -NoProfile -Command $Cmd
+
+
+
+$Cmd = "& {
+        cd '$Cwd'
+        `$Env:NVIM_PROFILE='$Profile'
+        `$Env:XDG_CONFIG_HOME=`"$CfgDir\nvim`"
+        nvim -i NONE -u '$CfgDir\nvim\init.lua'
+}"
+
+Write-Host $Profile
+Write-Host $Cwd
+Write-Host $CfgDir
+Write-Host $Cmd
 
 pwsh -NoProfile -Command $Cmd
+
+
+
+
+
+
+
+
+
+
+#pwsh -NoProfile -Command "& { & $Exec }" "$Profile" "$Cwd" "$CfgDir"
+
+#pwsh -NoProfile -Command "& { $Exec }" "$Profile" "$Cwd" "$CfgDir"
+
+return 
+
+# $Cmd = "`$Env:SANDBOX_NVIM_PWD='$CWD'; cd '$CfgDir'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u '$CfgDir\nvim\init.lua' --cmd 'set rtp+=$CfgDir/nvim'"
+
+Write-Verbose $Profile
+Write-Verbose $Cwd
+Write-Verbose $CfgDir
+
+$Cmd = "`$Env:NVIM_CONFIG='$CfgDir/nvim'; `$Env:NVIM_PROFILE='$Profile'; nvim --cmd 'lua vim.env.XDG_CONFIG_HOME=`'$CfgDir`'' --cmd 'cd `'$CWD`'"
+
+Write-Verbose $Cmd
+pwsh -NoProfile -Cmd $Cmd
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,10 @@
-require("generic.set")
-require("generic.cmds")
-require("system.hiyank")
-
 if os.getenv("NVIM_PROFILE") == "bare" then
     require("bare.init")
 elseif os.getenv("NVIM_PROFILE") == "sandbox" then
+    vim.env.XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
+
     print ("Sandbox mode: ".. vim.fn.getcwd())
+    print ("Config: ".. vim.env.XDG_CONFIG_HOME)
 
     -- Get the current runtime paths
     local rtp = vim.api.nvim_list_runtime_paths()
@@ -18,18 +17,28 @@ elseif os.getenv("NVIM_PROFILE") == "sandbox" then
         end
     end
 
+    table.insert(filtered_rtp, vim.env.XDG_CONFIG_HOME)
+    table.insert(filtered_rtp, vim.env.XDG_CONFIG_HOME .. "\\lua")
+
     -- Rebuild the runtime path
     vim.o.runtimepath = table.concat(filtered_rtp, ",")
-    print(vim.o.runtimepath)
 
-    vim.env.XDG_CONFIG_HOME = "../.sandbox/config"
-    vim.env.XDG_DATA_HOME = "../.sandbox/local/share"
-    vim.env.XDG_STATE_HOME = "../.sandbox/.local/state"
-    vim.env.XDG_CACHE_HOME = "../.sandbox/.cache"
+    vim.env.XDG_DATA_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/local/share"
+    vim.env.XDG_STATE_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/.local/state"
+    vim.env.XDG_CACHE_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/.cache"
 
+    local package_path = os.getenv("XDG_CONFIG_HOME") .. "\\"
+    package.path = package.path .. ';'
+    package.path = package.path .. package_path .. '?.lua;'
+    package.path = package.path .. package_path .. '?\\init.lua'
+
+    require("generic.set")
     require("generic.bootstrap_lazy")
 else
+    require("generic.set")
     require("generic.bootstrap_lazy")
 end
 
 require("lua.generic.remap")
+require("lua.generic.cmds")
+require("lua.system.hiyank")
