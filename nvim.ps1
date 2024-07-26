51bd14488347b9c7408ae60536345304668df04f
Roland Halbaksz <roland.halbaksz@semilab.hu>
2024-07-26 07:25:03 +0200

    Rename, local mode, cleanup.

--- nvim.ps1
+++ nvim.ps1
@@ -8,36 +8,6 @@ Param(
 $Cwd = "$PWD" #.Replace('\','\\') 
 $CfgDir = "$PSScriptRoot" #.Replace('\','\\')  
 
-# $Cmd = "`$Env:SANDBOX_NVIM_PWD='$CWD'; cd '$Path'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u '$Path\nvim\init.lua' --cmd 'set rtp+=$Path/nvim'"
-# $Cmd = "cd '$CWD';`$Env:NVIM_CONFIG='$Path/nvim'; `$Env:NVIM_PROFILE='sandbox'; nvim -i NONE --cmd 'lua vim.env.XDG_CONFIG_HOME=`'$Path`''"
-
-
-
-# $Cmd = "& {
-#     `$Exec = {
-#         `$Profile = `$args[0]
-#         `$Cwd = `$args[1]
-#         `$CfgDir = `$args[2]
-#         # `$Cmd = 'let `$XDG_CONFIG_HOME=`$CfgDir/nvim'
-#         `$Cmd = 'lua vim.env.XDG_CONFIG_HOME=`$CfgDir/nvim'
-# 
-# 
-#         Write-Host `$Profile
-#         Write-Host `$Cwd
-#         Write-Host `$CfgDir
-#         Write-Host `$Cmd
-# 
-#         cd `$Cwd; 
-#         `$ENV:NVIM_PROFILE=`$Profile; 
-#         nvim -i NONE --cmd `$Cmd;
-#     }
-#     & `$Exec '$Profile' '$Cwd' '$CfgDir'
-# }"
-# 
-# pwsh -NoProfile -Command $Cmd
-
-
-
 $Cmd = "& {
         cd '$Cwd'
         `$Env:NVIM_PROFILE='$Profile'
@@ -45,35 +15,9 @@ $Cmd = "& {
         nvim -i NONE -u '$CfgDir\nvim\init.lua'
 }"
 
-Write-Host $Profile
-Write-Host $Cwd
-Write-Host $CfgDir
-Write-Host $Cmd
-
-pwsh -NoProfile -Command $Cmd
-
-
-
-
-
-
-
-
-
-
-#pwsh -NoProfile -Command "& { & $Exec }" "$Profile" "$Cwd" "$CfgDir"
-
-#pwsh -NoProfile -Command "& { $Exec }" "$Profile" "$Cwd" "$CfgDir"
-
-return 
-
-# $Cmd = "`$Env:SANDBOX_NVIM_PWD='$CWD'; cd '$CfgDir'; `$ENV:NVIM_PROFILE='sandbox'; nvim -i NONE -u '$CfgDir\nvim\init.lua' --cmd 'set rtp+=$CfgDir/nvim'"
-
 Write-Verbose $Profile
 Write-Verbose $Cwd
 Write-Verbose $CfgDir
-
-$Cmd = "`$Env:NVIM_CONFIG='$CfgDir/nvim'; `$Env:NVIM_PROFILE='$Profile'; nvim --cmd 'lua vim.env.XDG_CONFIG_HOME=`'$CfgDir`'' --cmd 'cd `'$CWD`'"
-
 Write-Verbose $Cmd
-pwsh -NoProfile -Cmd $Cmd
+
+pwsh -NoProfile -Command $Cmd
--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ require("system.hiyank")
 
 if os.getenv("NVIM_PROFILE") == "bare" then
     require("bare.init")
-elseif os.getenv("NVIM_PROFILE") == "sandbox" then
+elseif os.getenv("NVIM_PROFILE") == "local" then
     vim.env.XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
     vim.env.XDG_DATA_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/local/share"
     vim.env.XDG_STATE_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/.local/state"
