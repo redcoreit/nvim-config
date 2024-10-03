c2f20fd59cff918bd57e1bfc2d6511192006ff08
Roland Halbaksz <redcoreit@gmail.com>
2024-10-03 07:12:00 +0200

    Use args correctly.

--- nvim.ps1
+++ nvim.ps1
@@ -1,9 +1,28 @@
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory = $false)] 
-    [string]
-    $Profile = "defalut"
+    [Parameter(ValueFromRemainingArguments = $true)] 
+    $Args
 )
+
+$Profile = "default"
+if ($Args) {
+    $ProfileIndex = [array]::IndexOf($Args, "--Profile")
+    if ($ProfileIndex -ge 0 -and $ProfileIndex + 1 -lt $Args.Count) {
+        # Extract the profile value
+        $Profile = $Args[$ProfileIndex + 1]
+
+        # Convert $Args to a List to allow removing elements
+        $ArgsList = [System.Collections.Generic.List[object]]::new()
+        $ArgsList.AddRange($Args)
+
+        # Remove --Profile and its associated value
+        $ArgsList.RemoveAt($ProfileIndex + 1) # Remove the profile value
+        $ArgsList.RemoveAt($ProfileIndex)     # Remove --Profile
+
+        # Convert back to array
+        $Args = $ArgsList.ToArray()
+    }
+}
 
 $Cwd = "$PWD" #.Replace('\','\\') 
 $CfgDir = "$PSScriptRoot" #.Replace('\','\\')  
@@ -12,7 +31,7 @@ $Cmd = "& {
         cd '$Cwd'
         `$Env:NVIM_PROFILE='$Profile'
         `$Env:XDG_CONFIG_HOME=`"$CfgDir`"
-        nvim -i NONE -u '$CfgDir\nvim\init.lua'
+        nvim -i NONE -u '$CfgDir\nvim\init.lua' $Args 
 }"
 
 Write-Verbose $Profile
--- nvim/init.lua
+++ nvim/init.lua
@@ -3,9 +3,18 @@ require("generic.remap")
 require("generic.cmds")
 require("system.hiyank")
 
-if os.getenv("NVIM_PROFILE") == "bare" then
+local profile = os.getenv("NVIM_PROFILE")
+
+if profile == "bare" then
     require("bare.init")
-elseif os.getenv("NVIM_PROFILE") == "local" then
+elseif profile == "local" then
+    vim.env.XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
+    vim.env.XDG_DATA_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/local/share"
+    vim.env.XDG_STATE_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/.local/state"
+    vim.env.XDG_CACHE_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/.cache"
+
+    require("generic.bootstrap_lazy")
+elseif profile == "default" then
     vim.env.XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME")
     vim.env.XDG_DATA_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/local/share"
     vim.env.XDG_STATE_HOME = vim.env.XDG_CONFIG_HOME .. "/.sandbox/.local/state"
@@ -16,3 +25,4 @@ else
     require("generic.bootstrap_lazy")
 end
 
+-- https://github.com/gbprod/yanky.nvim
