a1ca708704af9a8856984b10838df82c9dd181e0
Roland Halbaksz <roland.halbaksz@semilab.hu>
2025-04-23 14:44:00 +0200

    Fix depricated call.

--- nvim/lua/generic/lazy/lualine.lua
+++ nvim/lua/generic/lazy/lualine.lua
@@ -142,7 +142,7 @@ local opts_evil = function()
         function()
             local msg = '-'
             local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-            local clients = vim.lsp.get_active_clients()
+            local clients = vim.lsp.get_clients()
             if next(clients) == nil then
                 return msg
             end
